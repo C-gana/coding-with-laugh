435bd6f5791ebd9c150371a09c4ee997f106a114
C-gana <patiencebanda91@gmail.com>
2024-06-02 16:27:55 +0200

    done for the day

--- second session/practice.php
+++ second session/practice.php
@@ -9,7 +9,7 @@ $conn = new mysqli($server, $user, $pass, $db);
 if(!$conn){
   die();
 }
-
+/*
 $sql = "INSERT into users(username, email, password) values 
         ('Peace','peace@gmail.com','" . md5("peace") . "')";
 $result = $conn->query($sql);
@@ -18,4 +18,11 @@ if($result){
   echo "Data uploaded";
 }else{
   echo "mbola maguy";
+}
+*/
+
+$sql = "SELECT * from users";
+$result = $conn->query($sql);
+foreach($result as $row){
+  echo $row['username'] . " " . $row['email'] . " " . $row["password"] . "<br>";
 }
