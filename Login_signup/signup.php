3e8323e83498f4838b791ed906933ea87e66acaa
C-gana <patiencebanda91@gmail.com>
2024-07-18 23:48:24 +0200

    sign up front end done

--- Login_signup/signup.php
+++ Login_signup/signup.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
   <link rel="stylesheet" href="styles/style.css">
+  <link rel="stylesheet" href="styles/signup.css">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
@@ -11,11 +12,14 @@
     <div class="thumbnail">
     </div>
     <form method="post" class="card">
-      <h3>LOGIN</h3>
+      <h3>SIGN UP</h3>
       <input type="text" name="username" placeholder="Username">
+      <input type="email" name="email" placeholder="Email">
       <input type="password" name="pwd" placeholder="Password">
-      <input type="submit"  value="Login">
-      <div>Dont have account? <a href="signup.php">Sign Up</a></div>
+      <input type="password" name="pwd" placeholder="Confirm Password">
+      <input type="submit"  value="Sign up">
+      By clicking sign up you agree to our <br><a href="">Terms and conditions</a> .
+      <div>Already have account? <a href="login.php">Login</a></div>
     </form>
   </div>
 </body>
