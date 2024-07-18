a86b998d8379aa9df42a2f448b3ec6977abc06eb
C-gana <patiencebanda91@gmail.com>
2024-07-18 23:21:38 +0200

    login front end

---
+++ Login_signup/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="stylesheet" href="styles/style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+</head>
+<body>
+  <div class="container">
+    <div class="thumbnail">
+    </div>
+    <div class="card">
+      <h3>LOGIN</h3>
+      <input type="text" name="username" placeholder="Username">
+      <input type="password" name="pwd" placeholder="Password">
+      <input type="submit"  value="Login">
+      <div>Dont have account? <a href="signup.php">Sign Up</a></div>
+    </div>
+  </div>
+</body>
+</html>
