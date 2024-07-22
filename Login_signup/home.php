7db8756e13159a42ed9240a6300f4a380047faf4
C-gana <patiencebanda91@gmail.com>
2024-07-22 21:39:49 +0200

    finalizing the logout thing

--- Login_signup/home.php
+++ Login_signup/home.php
@@ -1,3 +1,29 @@
 <?php
 require ("auth.php");
-echo "LOGIN SUCCESSFUL";
+echo "LOGIN SUCCESSFUL<br>Welcome, $_SESSION[username]";?>
+<!DOCTYPE html>
+<html lang="en">
+<head><style>
+  body{
+    background:url(images/home.jpg);
+    background-size: cover;
+    background-attachment: fixed;
+  }
+  button{
+    background-color: darkorange;
+    border: none;
+    padding:10px 15px;
+  }
+  a{
+    text-decoration: none;
+    color: white;
+  }
+</style>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>HOME</title>
+</head>
+<body>
+    <button name="logout"><a href="logout.php">Logout</a></button>
+</body>
+</html>
